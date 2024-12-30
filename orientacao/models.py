2c55e2248c7b216f42ed2680f73181310695f6ab
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-12-30 18:10:44 -0300

    fix: adicionar novo campo de confirmação de orientação no modelo

--- orientacao/models.py
+++ orientacao/models.py
@@ -4,3 +4,4 @@ class Orientacao(models.Model):
   ID = models.AutoField(primary_key=True)
   fk_Usuario_ID_Aluno = models.ForeignKey('usuario.Usuario', on_delete=models.CASCADE, related_name='orientacao_aluno')
   fk_Usuario_ID_Professor = models.ForeignKey('usuario.Usuario', on_delete=models.CASCADE, related_name='orientacao_professor')
+  solicitacaoAceita = models.BooleanField(default=False)
