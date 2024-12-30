50b7f58abef1f82c07a9930208fc0b69d686a507
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-12-30 18:11:03 -0300

    fix: mudanças de permissão

--- atividade/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsTipo1(BasePermission):
-  def has_permission(self, request, view):
-    return request.user.is_authenticated and request.user.Usuario_TIPO == 1
