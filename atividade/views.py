72c4a6f7e7eb2a647759fcd56e1661cd99d4fb24
Pedro Luca Prates <pedrolucaofc@outlook.com>
2024-12-30 17:31:50 -0300

    feat: finalizar desenvolvimento do app atividade, bloqueio de rotas para usuários professores

--- atividade/views.py
+++ atividade/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Atividade
 from .serializers import AtividadeSerializer
+from .permissions import IsTipo1
 
 class AtividadeList(generics.ListCreateAPIView):
   queryset = Atividade.objects.all()
@@ -13,7 +14,7 @@ class AtividadeCreate(generics.CreateAPIView):
   queryset = Atividade.objects.all()
   serializer_class = AtividadeSerializer
   lookup_field = 'pk'
-  permission_classes = [IsAuthenticated]
+  permission_classes = [IsAuthenticated, IsTipo1]
 
 class AtividadeDetail(generics.RetrieveAPIView):
   queryset = Atividade.objects.all()
@@ -25,10 +26,10 @@ class AtividadeUpdate(generics.UpdateAPIView):
   queryset = Atividade.objects.all()
   serializer_class = AtividadeSerializer
   lookup_field = 'pk'
-  permission_classes = [IsAuthenticated]
+  permission_classes = [IsAuthenticated, IsTipo1]
 
 class AtividadeDelete(generics.DestroyAPIView):
   queryset = Atividade.objects.all()
   serializer_class = AtividadeSerializer
   lookup_field = 'pk'
-  permission_classes = [IsAuthenticated]
+  permission_classes = [IsAuthenticated, IsTipo1]
