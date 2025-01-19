48d8aa6df33a7528a776996699552d1704e95d87
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-01-18 21:44:23 -0300

    fix: attempt to fix issue with triplicated area de interesse

--- adicionar_areas.py
+++ adicionar_areas.py
@@ -1,11 +1,18 @@
+import os
+import django
+
+# Definindo as configurações do Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seu_projeto.settings")
+django.setup()
+
 from area_interesse.models import AreaInteresse
 
 areas = [
-    'Teste de software', 'Redes de Computadores', 'Agricultura Digital', 
-    'Algoritmos', 'Análise e Projeto de Sistemas', 'Aprendizado de Máquina', 
-    'Avaliação de Desempenho de Sistemas', 'Banco de Dados', 'Desenvolvimento para Dispositivos Móveis', 
-    'Desenvolvimento Multiplataforma', 'Desenvolvimento Web', 'DevOps', 'E-Learning', 
-    'Engenharia de Software', 'Estatística', 'Estrutura de Dados', 'Gestão de Tecnologia da Informação', 
+    'Teste de software', 'Redes de Computadores', 'Agricultura Digital',
+    'Algoritmos', 'Análise e Projeto de Sistemas', 'Aprendizado de Máquina',
+    'Avaliação de Desempenho de Sistemas', 'Banco de Dados', 'Desenvolvimento para Dispositivos Móveis',
+    'Desenvolvimento Multiplataforma', 'Desenvolvimento Web', 'DevOps', 'E-Learning',
+    'Engenharia de Software', 'Estatística', 'Estrutura de Dados', 'Gestão de Tecnologia da Informação',
     'Inclusão Digital'
 ]
 
