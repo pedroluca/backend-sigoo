180b0fcf59fdf5195260a01cca8be02ec170196b
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-01-26 11:04:45 -0300

    feat: add new feature to block changing password to same

--- usuario/serializers.py
+++ usuario/serializers.py
@@ -40,31 +40,38 @@ class UsuarioSerializer(serializers.ModelSerializer):
         return user
 
 class UsuarioUpdateSerializer(serializers.ModelSerializer):
-  area_interesse = serializers.PrimaryKeyRelatedField(queryset=AreaInteresse.objects.all(), many=True, required=False)
-  password = serializers.CharField(write_only=True, required=False)
+    area_interesse = serializers.PrimaryKeyRelatedField(queryset=AreaInteresse.objects.all(), many=True, required=False)
+    password = serializers.CharField(write_only=True, required=False)
 
-  class Meta:
-    model = Usuario
-    fields = '__all__'
+    class Meta:
+        model = Usuario
+        fields = '__all__'
 
-    def update(self, instance, validated_data):
-      if 'username' in validated_data:
-          instance.username = validated_data.get('username', instance.username)
-      if 'email' in validated_data:
-          instance.email = validated_data.get('email', instance.email)
-      if 'nome' in validated_data:
-          instance.nome = validated_data.get('nome', instance.nome)
-      if 'matricula' in validated_data:
-          instance.matricula = validated_data.get('matricula', instance.matricula)
-      if 'Usuario_TIPO' in validated_data:
-          instance.Usuario_TIPO = validated_data.get('Usuario_TIPO', instance.Usuario_TIPO)
-      if 'password' in validated_data:
-          instance.set_password(validated_data['password'])
-      if 'area_interesse' in validated_data:
-          instance.area_interesse.set(validated_data['area_interesse'])
-      if 'quantidade_orientandos' in validated_data:
-          instance.quantidade_orientandos = validated_data.get('quantidade_orientandos', instance.quantidade_orientandos)
-      if 'tema' in validated_data:
-          instance.tema = validated_data.get('tema', instance.tema)
-      instance.save()
-      return instance
+        def update(self, instance, validated_data):
+            if 'username' in validated_data:
+                instance.username = validated_data.get('username', instance.username)
+            if 'email' in validated_data:
+                instance.email = validated_data.get('email', instance.email)
+            if 'nome' in validated_data:
+                instance.nome = validated_data.get('nome', instance.nome)
+            if 'matricula' in validated_data:
+                instance.matricula = validated_data.get('matricula', instance.matricula)
+            if 'Usuario_TIPO' in validated_data:
+                instance.Usuario_TIPO = validated_data.get('Usuario_TIPO', instance.Usuario_TIPO)
+            if 'password' in validated_data:
+                instance.set_password(validated_data['password'])
+            if 'area_interesse' in validated_data:
+                instance.area_interesse.set(validated_data['area_interesse'])
+            if 'quantidade_orientandos' in validated_data:
+                instance.quantidade_orientandos = validated_data.get('quantidade_orientandos', instance.quantidade_orientandos)
+            if 'tema' in validated_data:
+                instance.tema = validated_data.get('tema', instance.tema)
+            if 'password' in validated_data:
+                if Usuario.objects.filter(instance=instance).exists():
+                    print(instance.password)
+                    if instance.password != validated_data['password']:
+                        instance.set_password(validated_data['password'])
+                    else:
+                        raise serializers.ValidationError("A nova senha não pode ser igual a anterior.")
+            instance.save()
+            return instance
