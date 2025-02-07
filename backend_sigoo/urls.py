427ff456778f79426240897b8633c7d7a1df6f4b
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-02-07 17:59:15 -0300

    feat: add index page to / route

--- backend_sigoo/urls.py
+++ backend_sigoo/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_rest.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),  # Adiciona esta linha
 ]
 
 if settings.DEBUG:
