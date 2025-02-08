08913d4689934c1c409832be0df6a24f0985b07b
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-02-08 13:10:44 -0300

    build: add new allowed host

--- backend_sigoo/settings.py
+++ backend_sigoo/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = 'django-insecure-*0bh^@4r#78trb+gmiykwel5ulzfgh#4!#r#7ulu(#4wde235#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://backend-sigoo.netlify.app']
-
+ALLOWED_HOSTS = ['https://api-sigoo.tech', 'https://www.api-sigoo.tech']
 
 # Application definition
 
@@ -156,7 +155,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_ORIGINS = [
-  'http://localhost:5173/',
-  'https://frontend-sigoo.vercel.app',
-  'https://frontend-sigoo.netlify.app',
+#   'http://localhost:5173/',
+    'https://api-sigoo.tech',
+    'https://www.api-sigoo.tech',
 ]
